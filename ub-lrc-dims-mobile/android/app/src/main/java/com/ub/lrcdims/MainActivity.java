df1e42d1351cf87f371599ba5aedf02616e602a7
Ferdinand420 <2201765@ub.edu.ph>
2025-12-07 17:52:19 +0800

    mobile v1.0.5

--- ub-lrc-dims-mobile/android/app/src/main/java/com/ub/lrcdims/MainActivity.java
+++ ub-lrc-dims-mobile/android/app/src/main/java/com/ub/lrcdims/MainActivity.java
@@ -1,5 +1,13 @@
 package com.ub.lrcdims;
 
+import android.os.Bundle;
+import android.util.Log;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    Log.d("MainActivity", "onCreate has been called");
+  }
+}
